2c5e2969b3da82cf8b0bf597feff9e1cd42ae2fa
Matthew Hope <matt@matthewhope.co.uk>
2023-07-04 00:32:14 +0100

    Implement program

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char argv[])
+{
+    int choice = 0;
+    do
+    {
+        // Prompt user for input
+        system("clear");
+        printf("=== Choose ===\n");
+        printf("\n[1] - Continue\n");
+        printf("\n[2] - Exit\n");
+        // Capture input
+        char buf[BUFSIZ];
+        if (fgets(buf, BUFSIZ, stdin) == NULL)
+        {
+            perror("fgets");
+            return EXIT_FAILURE;
+        }
+        int selection = atoi(buf);
+        switch (selection)
+        {
+            case 1:
+                break;
+            case 2:
+                choice = 1;
+                break;
+            default:
+                printf("Invalid Selection. Press enter to try again. ");
+                while (getchar() != '\n') ;
+                break;
+        }
+    } while (!choice) ;
+    return EXIT_FAILURE;
+}
